4b47a02b16bfc9665fededc337a9a933a10e863e
Asghar <ma.asghar02@gmail.com>
2022-09-22 12:36:05 +0500

    first comment

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Value of A is: "+ a);
+        int b = sc.nextInt();
+        System.out.println("Value of B is: "+ b);
+        Main m = new Main();
+        m.swap(a,b);
+
+
+
+        System.out.println("Hello world!");
+    }
+
+    public void swap(int a, int b){
+        int temp;
+        temp = b;
+        b = a;
+        a = temp;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
