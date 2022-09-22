cddeb43d4cee81c920fecdf3dd7f63d761916995
Asghar Test <ma.asghar02@gmail.com>
2022-09-23 00:51:31 +0500

    Stored first test

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Java\\Browser Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://bystored:bystored2020@stage.bystored.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("postcode")).sendKeys("BN9");
+        driver.findElement(By.id("get-storage-price-v-35")).click();
+//        driver.manage().timeouts().implicitlyWait(20000, tim.SECONDS);
+        Thread.sleep(5000);
+
+        driver.close();
+        driver.quit();
+
+
+    }
+}
